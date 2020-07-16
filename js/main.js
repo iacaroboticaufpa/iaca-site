684739b7672bac54849b4333431e86802f4cfb71
Gabriela Souza <gabi14info@gmail.com>
2020-07-16 11:43:55 -0300

    Continuacao das alteraçoes na secao equipe(criacao da estrutura do lado traseiro e adicao do flip), formatacao do index, exclusao dos arquivos que nao estao sendo mais utilizados

--- js/main.js
+++ js/main.js
@@ -78,55 +78,7 @@
 	onePageClick();
 	
 
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-
-		$('.carousel-testimony').owlCarousel({
-			center: true,
-			loop: true,
-			autoplay: true,
-			autoplaySpeed:2000,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-	};
-	carousel();
-
+	
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
 		// 	 timer;
